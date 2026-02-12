a50ee844141acbcf960f4866e5a0f24f2dad54e7
Patrick Dillon <padillon@redhat.com>
2026-02-12 06:05:48 -0500

    pkg/types: default CAPI machine management
    Defaults the machine pool management to CAPI when the appropriate
    feature gate is enabled.

--- pkg/types/defaults/machinepools.go
+++ pkg/types/defaults/machinepools.go
@@ -1,18 +1,20 @@
 package defaults
 
 import (
+	"github.com/openshift/api/features"
 	"github.com/openshift/installer/pkg/types"
 	"github.com/openshift/installer/pkg/types/aws"
 	awsdefaults "github.com/openshift/installer/pkg/types/aws/defaults"
 	"github.com/openshift/installer/pkg/types/azure"
 	azuredefaults "github.com/openshift/installer/pkg/types/azure/defaults"
+	"github.com/openshift/installer/pkg/types/featuregates"
 	"github.com/openshift/installer/pkg/types/gcp"
 	gcpdefaults "github.com/openshift/installer/pkg/types/gcp/defaults"
 	"github.com/openshift/installer/pkg/version"
 )
 
 // SetMachinePoolDefaults sets the defaults for the machine pool.
-func SetMachinePoolDefaults(p *types.MachinePool, platform *types.Platform) {
+func SetMachinePoolDefaults(p *types.MachinePool, platform *types.Platform, fgates featuregates.FeatureGate) {
 	defaultReplicaCount := int64(3)
 	if p.Name == types.MachinePoolEdgeRoleName || p.Name == types.MachinePoolArbiterRoleName {
 		defaultReplicaCount = 0
@@ -25,6 +27,16 @@ func SetMachinePoolDefaults(p *types.MachinePool, platform *types.Platform) {
 	}
 	if p.Architecture == "" {
 		p.Architecture = version.DefaultArch()
+	}
+
+	// Set management to ClusterAPI if the appropriate feature gate is enabled and management is unspecified
+	// TODO(padillon): clarify whether edge node support can be included with compute.
+	if p.Management == "" {
+		if p.Name == types.MachinePoolControlPlaneRoleName && fgates.Enabled(features.FeatureGateClusterAPIControlPlaneInstall) {
+			p.Management = types.ClusterAPI
+		} else if p.Name == types.MachinePoolComputeRoleName && fgates.Enabled(features.FeatureGateClusterAPIComputeInstall) {
+			p.Management = types.ClusterAPI
+		}
 	}
 
 	switch platform.Name() {
@@ -57,7 +69,7 @@ func hasEdgePoolConfig(pools []types.MachinePool) bool {
 }
 
 // CreateEdgeMachinePoolDefaults create the edge compute pool when it is not already defined.
-func CreateEdgeMachinePoolDefaults(pools []types.MachinePool, platform *types.Platform, replicas int64) *types.MachinePool {
+func CreateEdgeMachinePoolDefaults(pools []types.MachinePool, platform *types.Platform, replicas int64, fgates featuregates.FeatureGate) *types.MachinePool {
 	if hasEdgePoolConfig(pools) {
 		return nil
 	}
@@ -65,6 +77,6 @@ func CreateEdgeMachinePoolDefaults(pools []types.MachinePool, platform *types.Pl
 		Name:     types.MachinePoolEdgeRoleName,
 		Replicas: &replicas,
 	}
-	SetMachinePoolDefaults(pool, platform)
+	SetMachinePoolDefaults(pool, platform, fgates)
 	return pool
 }
